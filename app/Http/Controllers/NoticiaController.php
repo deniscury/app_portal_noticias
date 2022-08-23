5ff602b2b234cbb56b1de31419208be313cf4acb
denim95 <99043807+denim95@users.noreply.github.com>
2022-08-23 18:43:06 -0300

    Aula 23/08/2022 - Redis

---
+++ app/Http/Controllers/NoticiaController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Noticia;
+use App\Http\Requests\StoreNoticiaRequest;
+use App\Http\Requests\UpdateNoticiaRequest;
+use Illuminate\Support\Facades\Cache;
+
+class NoticiaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        /*
+            // Criar um dado dentro do bd Redis
+            Cache::put('site', 'google.com.br', 10);
+
+            // Recuperar dado dentro do Redis
+            $site = Cache::get('site');
+
+            if (!Cache::has('dez_noticias')){
+                $noticias = 
+                Cache::put('dez_noticias', $noticias, 15);
+            }
+
+            $noticias = Cache::get('dez_noticias');
+        */
+        
+        $noticias = [];
+        
+        $noticias = Cache::remember('dez_noticias', 15, function(){
+            return Noticia::orderByDesc('created_at')->limit(10)->get();
+        });
+
+        return view('noticia', ['noticias' => $noticias]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreNoticiaRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreNoticiaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Noticia  $noticia
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Noticia $noticia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Noticia  $noticia
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Noticia $noticia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateNoticiaRequest  $request
+     * @param  \App\Models\Noticia  $noticia
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateNoticiaRequest $request, Noticia $noticia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Noticia  $noticia
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Noticia $noticia)
+    {
+        //
+    }
+}
---
+++ database/seeders/NoticiaSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Noticia;
+
+class NoticiaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        ini_set('memory_limit', '512M');
+        Noticia::factory(1000)->create();
+    }
+}
